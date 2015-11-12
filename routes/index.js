b893f1a669c82ccc697398e2d990a912bd52c9f6
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-12 22:57:39 +0000

    solucionado el problema en routes/index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
-router.get('/', quizController.index);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+});
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
