b377c1bc076c79bad7f045c52ce7d236223b0f03
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-12 23:16:09 +0000

    añadido e-partials en app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users');
-
+var partials = require('express-partials');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use(partials());
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
