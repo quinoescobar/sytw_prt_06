8a05c28aa82605d2424c6e809ab4fcb32f33a4a9
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-12 23:42:21 +0000

    controller mejorado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,24 @@
+var Quiz = require('../models/quiz_model');
+
+var quiz = new Quiz();
+var current = quiz.randomQuestion();
+
+exports.index = function(req, res, next) {
+  debug("Yendo a  index.js:  '/'");
+  res.render('index', { title: 'Quiz' });
+};
+
 /* GET quizes/question page. */
-exports.question = function(req, res){
-  res.render('quizes/question',{pregunta: 'Capital de Italia'});
+exports.question = function(req,res) {
+  current = quiz.randomQuestion();
+  res.render('quizes/question', {pregunta: current.pregunta});
 };
 
 /* GET quizes/answer page. */
-exports.answer = function(req, res){
-  if(req.query.respuesta == 'Roma'){
-    res.render('quizes/answer',{respuesta: 'Correcto'});
-  }else {
-    res.render('quizes/answer',{respuesta: 'Incorrecto'});
-  }
+exports.answer = function(req, res) {
+  var c = 'Incorrecto';
+  debug(req.query);
+  debug("current.respuesta(req.query.respuesta) = "+current.respuesta(req.query.respuesta));
+  if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
+  res.render('quizes/answer', {respuesta: c});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,5 @@ router.get('/', function(req, res, next) {
 });
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-
+router.get('/quizes/questions', quizController.questions);
 module.exports = router;
