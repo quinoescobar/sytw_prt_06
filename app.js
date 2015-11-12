0f00ef8671dcda32e60ee48b72ec679cf963d30d
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-12 23:18:08 +0000

    cambiado el orden en el app.js

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = require('express-partials');
 var routes = require('./routes/index');
 // var users = require('./routes/users');
-var partials = require('express-partials');
 var app = express();
 
 // view engine setup
@@ -22,8 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use(partials());
+app.use('/', routes);
+
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
